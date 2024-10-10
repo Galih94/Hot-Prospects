aea180b4d9f34186c6f0838bf7c9d46d4a6f3767
galih <samudragalih94@gmail.com>
2024-10-10 09:49:17 +0700

    feat(Selection): create List with tappable item and set/save value selected

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -8,14 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection: String?
+    var users: [String] = ["Tohru", "Yuki", "Kyo", "Momiji"]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if let selection {
+                Text("you selected \(selection)")
+            }
+            List(users, id: \.self, selection: $selection) { user in
+                Text(user)
+            }
         }
-        .padding()
     }
 }
 
