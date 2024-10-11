9e6942370d35201f5c19cf22736a6a7f98b1e8b3
galih <samudragalih94@gmail.com>
2024-10-11 12:52:02 +0700

    clean code

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -7,32 +7,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var output = ""
-    
     var body: some View {
-        Text(output)
-            .task {
-                await fetchReadings()
-            }
-    }
-    
-    func fetchReadings() async {
-        let fetchTask = Task {
-            let url = URL(string: "https://hws.dev/readings.json")!
-            let (data, _) = try await URLSession.shared.data(from: url)
-            let readings = try JSONDecoder().decode([Double].self, from: data)
-            return """
-                Found \(readings.count) count
-                \(readings)
-                """
-        }
-        let result = await fetchTask.result
-        switch result {
-        case .success(let success):
-            output = success
-        case .failure(let failure):
-            output = "error -- \(failure.localizedDescription)"
-        }
+        Text("Hello world")
     }
 }
 
