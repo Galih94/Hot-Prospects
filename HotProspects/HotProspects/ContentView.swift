1f040c655ec9c5c053fb68941f289b840f6144d7
galih <samudragalih94@gmail.com>
2024-10-11 10:03:13 +0700

    clean code

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -4,31 +4,13 @@
 //
 //  Created by Galih Samudra on 10/10/24.
 //
-
 import SwiftUI
 
 struct ContentView: View {
-    @State private var selectedTab = "One"
     var body: some View {
-        TabView(selection: $selectedTab) {
-            Button("Show tab 2") {
-                selectedTab = "Two"
-            }
-            .tabItem {
-                Label("One", systemImage: "star")
-            }
-            .tag("One")
-            Button("Show tab 1") {
-                selectedTab = "One"
-            }
-            .tabItem {
-                Label("Two", systemImage: "circle")
-            }
-            .tag("Two")
-        }
+        Text("Hello world")
     }
 }
-
 
 #Preview {
     ContentView()
