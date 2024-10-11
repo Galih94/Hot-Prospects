d38c0b95a2027850c9e869babe742df8a577d9d4
galih <samudragalih94@gmail.com>
2024-10-11 10:12:08 +0700

    feat(Data): example fetch data from internet

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -7,8 +7,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var output = ""
+    
     var body: some View {
-        Text("Hello world")
+        Text(output)
+            .task {
+                await fetchReadings()
+            }
+    }
+    
+    func fetchReadings() async {
+        do {
+            let url = URL(string: "https://hws.dev/readings.json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let readings = try JSONDecoder().decode([Double].self, from: data)
+            output = """
+                Found \(readings.count) count
+                \(readings)
+                """
+        } catch {
+            print("error -- \(error.localizedDescription)")
+        }
     }
 }
 
