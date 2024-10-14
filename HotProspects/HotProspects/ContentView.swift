1e6f19e7d788d584fb930054ea8329d2eb5367db
galih <samudragalih94@gmail.com>
2024-10-14 16:36:57 +0700

    feat(SwiftPackageManager): import SamplePackage using SwiftPackageManager

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -4,11 +4,19 @@
 //
 //  Created by Galih Samudra on 10/10/24.
 //
+import SamplePackage
 import SwiftUI
 
 struct ContentView: View {
+    private let possibleNumbers = 1...60
+    private var results: String {
+        let selected = possibleNumbers.random(7).sorted()
+        let strings = selected.map {"\($0)"}
+        
+        return strings.formatted()
+    }
     var body: some View {
-        Text("Hello world")
+        Text(results)
     }
 }
 
