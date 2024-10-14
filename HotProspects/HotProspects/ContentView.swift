fe78a4e3b973a36b804f7040c1749ca42f5f9fbb
galih <samudragalih94@gmail.com>
2024-10-14 16:04:22 +0700

    clean code

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -5,41 +5,10 @@
 //  Created by Galih Samudra on 10/10/24.
 //
 import SwiftUI
-import UserNotifications
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Button("Request permission") {
-                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { isSuccess, error in
-                    if isSuccess {
-                        print("success requestAuthorization")
-                    } else if let error {
-                        print("error -- \(error.localizedDescription)")
-                    }
-                }
-            }
-            
-            Button("Schedule notification") {
-                /// notification content
-                let content = UNMutableNotificationContent()
-                content.title = "Feed the cat"
-                content.subtitle = "it looks hungry"
-                content.sound = UNNotificationSound.default
-                
-                /// show 5 second from now
-                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-                
-                
-                /// create local notif with random UUID
-                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-                
-                /// add notif to request
-                UNUserNotificationCenter.current().add(request)
-                
-                ///NOTE:  cmd+L on simulator to lock screen
-            }
-        }
+        Text("Hello world")
     }
 }
 
