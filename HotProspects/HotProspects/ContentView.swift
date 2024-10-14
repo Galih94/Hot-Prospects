cc0d19190c0846106320ae7b687fdf8323be4a84
galih <samudragalih94@gmail.com>
2024-10-14 14:48:16 +0700

    clean code

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -7,26 +7,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var backgroundColor = Color.red
     var body: some View {
-        VStack {
-            Text("Hello world")
-                .padding()
-                .background(backgroundColor)
-            Text("Change Color")
-                .padding()
-                .contextMenu{
-                    Button("Red", systemImage: backgroundColor == .red ? "checkmark.circle.fill" : "", role: .destructive) {
-                        backgroundColor = Color.red
-                    }
-                    Button("Green", systemImage: backgroundColor == .green ? "checkmark.circle.fill" : "", role: .none) {
-                        backgroundColor = Color.green
-                    }
-                    Button("Blue", systemImage: backgroundColor == .blue ? "checkmark.circle.fill" : "", role: .cancel) {
-                        backgroundColor = Color.blue
-                    }
-                }
-        }
+        Text("Hello world")
     }
 }
 
