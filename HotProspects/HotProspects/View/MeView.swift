2d52076a876b8c57eed3efbd8b6be86bcef71a0b
galih <samudragalih94@gmail.com>
2024-10-15 19:40:49 +0700

    feat(TabView): create Tabview with 4 tab item

---
+++ HotProspects/HotProspects/View/MeView.swift
@@ -0,0 +1,18 @@
+//
+//  MeView.swift
+//  HotProspects
+//
+//  Created by Galih Samudra on 15/10/24.
+//
+
+import SwiftUI
+
+struct MeView: View {
+    var body: some View {
+        Text("Me View")
+    }
+}
+
+#Preview {
+    MeView()
+}
---
+++ HotProspects/HotProspects/View/ProspectsView.swift
@@ -0,0 +1,18 @@
+//
+//  ProspectsView.swift
+//  HotProspects
+//
+//  Created by Galih Samudra on 15/10/24.
+//
+
+import SwiftUI
+
+struct ProspectsView: View {
+    var body: some View {
+        Text("Prospect View")
+    }
+}
+
+#Preview {
+    ProspectsView()
+}
