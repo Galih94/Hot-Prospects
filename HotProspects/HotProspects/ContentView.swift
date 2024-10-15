0e3d48b60bd7ab1c4ec8f80092fd42e73b7d6c6d
galih <samudragalih94@gmail.com>
2024-10-15 19:33:58 +0700

    clean code

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -4,19 +4,11 @@
 //
 //  Created by Galih Samudra on 10/10/24.
 //
-import SamplePackage
 import SwiftUI
 
 struct ContentView: View {
-    private let possibleNumbers = 1...60
-    private var results: String {
-        let selected = possibleNumbers.random(7).sorted()
-        let strings = selected.map {"\($0)"}
-        
-        return strings.formatted()
-    }
     var body: some View {
-        Text(results)
+        Text("Hello world")
     }
 }
 
