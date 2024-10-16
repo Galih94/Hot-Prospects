78ab42eb8a75d1de44eb8ab8185a09ab5efbf10a
galih <samudragalih94@gmail.com>
2024-10-16 19:36:40 +0700

    feat(MeView): input textfield for name and email and save to local app storage then create QR code from name and email

--- HotProspects/HotProspects/View/MeView.swift
+++ HotProspects/HotProspects/View/MeView.swift
@@ -5,11 +5,47 @@
 //  Created by Galih Samudra on 15/10/24.
 //
 
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct MeView: View {
+    @AppStorage("name") private var name = "Anonymus"
+    @AppStorage("emailAddress") private var emailAddress = "yourName@email.com"
+    
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
     var body: some View {
-        Text("Me View")
+        NavigationStack {
+            VStack {
+                Form {
+                    TextField("Name", text: $name)
+                        .textContentType(.name)
+                        .font(.title)
+                    
+                    TextField("Email Address", text: $emailAddress)
+                        .textContentType(.emailAddress)
+                        .font(.title)
+                    
+                    Image(uiImage: generatedQRCode(from: "\(name)\n\(emailAddress)"))
+                        .interpolation(.none)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200, height: 200)
+                }
+                .navigationTitle("Your Code")
+            }
+        }
+    }
+    
+    func generatedQRCode(from string: String) -> UIImage {
+        filter.message = Data(string.utf8)
+        
+        if let outputImage = filter.outputImage,
+            let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+            return UIImage(cgImage: cgImage)
+        }
+        
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
     }
 }
 
